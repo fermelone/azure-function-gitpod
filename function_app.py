b7cd4876a5ca95c76a6ab81df3b84f1e4fd09120
Fern <fermelone@gmail.com>
2024-02-16 11:10:43 +0000

    Update function_app.py
    Adding Gitpod logo to response

--- function_app.py
+++ function_app.py
@@ -17,7 +17,25 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(f"""
+            .---:.            
+        .:--------            
+     :-======----:            
+  :==========-:.              
+.=========-:.        .:-----: 
+=======-.         :-===------:
+======:       .:-=========----
++++===:     :================-
+++++++:     ========-. :======
+++++++:     .-==-:.    :======
+++++++-                :======
+=++++++=-.          .:=======-
+ =+++++++++=:.   :-========== 
+  :-++++++++++==++++++====-.  
+     .-=++++++++++++++=:.     
+         :-++++++++-:         
+
+                                 \nHello, {name}. \nThis HTTP triggered function is running in Gitpod!""")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
